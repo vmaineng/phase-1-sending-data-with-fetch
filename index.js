d460fc47f5201869f16cb109826ab1b9e04198b4
Mai Vang <vmaineng@gmail.com>
2022-05-06 10:43:59 -0700

    commit tree 07677873dc901c890224a8314d4d0a7900b9f47b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email){
+    fetch('http://localhost:3000/users', {
+        method:'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name:'Steve', 
+            email: 'steve@steve.com',
+        }),
+    })
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (object) {
+        console.log(object);
+      })
+    .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+      });
+}
